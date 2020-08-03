a0e6e9259acc2473fde7cc9a5f3fe0798ee37836
danny17495 <danny17495@gmail.com>
2020-08-03 11:51:37 +0800

    環境變數 基本設定

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,42 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    //'https://vue-course-api.hexschool.io/api/danny17495/products';
+    // api 伺服器路徑
+    // 申請的API PATH
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH)
+    this.$http.get(api).then((response) => {
+      console.log(response.data);
+    });
+  },
 }
 </script>
 
-<style>
-#app {
+<style lang="scss">
+@import "./assets/all.scss";
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
