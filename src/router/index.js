a77056ea93cd051d12b847b4a553b9930aaf5737
danny17495 <danny17495@gmail.com>
2020-08-04 13:47:54 +0800

    新增導航守衛

--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,19 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
+    //若進入未編寫之頁面，強制進入登入頁面
+    {
+      path: '*',
+      redirect: 'login',
+    },
+    //首頁
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: {requiresAuth: true},
     },
+    //登入頁面
     {
       path: '/login',
       name: 'Login',
