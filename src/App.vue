c99cb4dc6dd106e2b706ebf5af59aa0c3ca50ac2
danny17495 <danny17495@gmail.com>
2020-08-06 14:07:05 +0800

    新增產品內容 抓取ＡＰＩ

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,6 @@
 <script>
 export default {
   name: 'App',
-  created() {
-    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
-    //'https://vue-course-api.hexschool.io/api/danny17495/products';
-    // api 伺服器路徑
-    // 申請的API PATH
-    console.log(process.env.APIPATH, process.env.CUSTOMPATH)
-    this.$http.get(api).then((response) => {
-      console.log(response.data);
-    });
-  },
 }
 </script>
 
