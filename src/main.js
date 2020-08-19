e36ac83235e425051713f1014ffd9c6057501669
danny17495 <danny17495@gmail.com>
2020-08-19 10:16:07 +0800

    新增酷碰券

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import App from './App';
 import router from './router';
 import './bus';
 import currencyFilter from './filters/currency';
+import dateFilter from './filters/date';
 
 
 Vue.config.productionTip = false
@@ -18,6 +19,7 @@ Vue.use(VueAxios, axios);
 
 Vue.component('Loading', Loading);
 Vue.filter('currency', currencyFilter);
+Vue.filter('date', dateFilter);
 
 axios.defaults.withCredentials = true;
 /* eslint-disable no-new */
