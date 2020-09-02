51802ff0a31924f23dafeaa1627bf61e06ba416e
danny17495 <danny17495@gmail.com>
2020-09-02 14:36:32 +0800

    修改樣式 商品跳動 標籤 打字機效果

--- src/components/pages/front/Subscription.vue
+++ src/components/pages/front/Subscription.vue
@@ -1,26 +1,21 @@
-  
-<template>
-  <div>
-    <section class="py-4 bg-sub">
-    <div class="container">
+  <template>
+    <section class="py-4 bg-sub" data-aos="fade-right" >
+      <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-5 d-flex align-items-center justify-content-center justify-content-md-start mb-4 mb-md-0">
-                <img src="" alt="" width="35px" height="32px">
-                <span class="h5 mx-2 mb-0 subtext">訂閱優惠信息</span>
+          <div class="col-md-5 d-flex align-items-center justify-content-center justify-content-md-start mb-4 mb-md-0">
+            <img src="" alt="" width="35px" height="32px">
+            <span class="h5 mx-2 mb-0 subtext">訂閱優惠信息</span>
+          </div>
+          <div class="col-md-5">
+            <div class="form-square form-square-withicon d-flex justify-content-between">
+              <label for="subscription" class="form-square-icon-left"><i class="fas fa-envelope"></i></label>
+              <input type="email" class="form-input w-100" id="subscription" placeholder="請輸入E-mail">
+              <button class="btn btn-accent btn-lg d-flex align-items-center">
+                <i class="fas fa-angle-right fa-2x"></i>
+              </button>
             </div>
-            <div class="col-md-5">
-                <div class="form-square form-square-withicon d-flex justify-content-between">
-                    <label for="subscription" class="form-square-icon-left"><i class="fas fa-envelope"></i></label>
-                    <input type="email" class="form-input w-100" id="subscription" placeholder="請輸入E-mail">
-                    <button class="btn btn-accent btn-lg d-flex align-items-center">
-                        <i class="fas fa-angle-right fa-2x"></i>
-                    </button>
-                </div>
-            </div>
+          </div>
         </div>
-    </div>
-</section>
-
-
-  </div>
-</template>
+      </div>
+    </section>
+  </template>
